365c0b08d7b51f6282bd4997a3c634124c1150ae
SebasHerPorras <hernandezporrassebastian@gmail.com>
2025-08-26 11:16:13 -0600

    pagina en vue
    tutorial completado de vue js

---
+++ Laboratorio3_C23770/mi primer proyecto/frontend-lab/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ListaPaises :countries="countries" @eliminar="eliminarPais" />
+</template>
+
+<script>
+import ListaPaises from "./components/CountriesList.vue";
+
+export default {
+  components: {
+    ListaPaises,
+  },
+  data() {
+    return {
+      countries: [
+        { name: "Costa Rica", continent: "América", language: "Español" },
+        { name: "Japón", continent: "Asia", language: "Japonés" },
+        { name: "Corea del Sur", continent: "Asia", language: "Coreano" },
+        { name: "Italia", continent: "Europa", language: "Italiano" },
+        { name: "Alemania", continent: "Europa", language: "Alemán" },
+      ],
+    };
+  },
+  methods: {
+    eliminarPais(nombre) {
+      this.countries = this.countries.filter((country) => country.name !== nombre);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
