eee26879608d7b3966dd8c65877a31d27f451b47
SebasHerPorras <hernandezporrassebastian@gmail.com>
2025-09-02 23:12:51 -0600

    Lab4

---
+++ Laboratorio4_C23770/backend-lab-C23770/backend-lab-C23770/Services/CountryService.cs
@@ -0,0 +1,20 @@
+﻿
+using backend_lab_C23770.Models;
+using backend_lab_C23770.Repositories;
+
+namespace backend_lab_C23770.Services
+{
+    public class CountryService
+    {
+        private readonly CountryRepository countryRepository;
+        public CountryService()
+        {
+            countryRepository = new CountryRepository();
+        }
+        public List<CountryModel> GetCountries()
+        {
+            // Add any missing business logic when it is neccesary
+        return countryRepository.GetCountries();
+        }
+    }
+}
